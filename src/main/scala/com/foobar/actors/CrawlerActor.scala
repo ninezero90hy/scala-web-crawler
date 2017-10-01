c6c20789a7ab27d5ab5a5d18cf9b10b127937310
Shivansh <shiv4nsh@gmail.com>
2017-10-01 13:48:44 +0530

    Reduced the complexity by filtering out the data

--- src/main/scala/com/foobar/actors/CrawlerActor.scala
+++ src/main/scala/com/foobar/actors/CrawlerActor.scala
@@ -17,27 +17,28 @@ class CrawlerActor extends Actor {
   override def receive: Receive = {
 
     case Visit(url: URL) =>
-      println(s"url::${url.toString}")
-        if (!SiteMap.tree.contains(url.toString) && url.toString.contains("wiprodigital.com") && !(url.toString.contains("facebook")) &&
-            !(url.toString.contains("twitter")) && !(url.toString.contains("linkedin"))) {
-          val response = Jsoup.connect(url.toString).ignoreContentType(true)
-            .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1").execute()
-          val contentType: String = response.contentType
-          if (contentType.startsWith("text/html")) {
-            val doc = response.parse()
-            val title: String = doc.getElementsByTag("title").asScala.map(e => e.text()).head
-            val descriptionTag = doc.getElementsByTag("meta").asScala.filter(e => e.attr("name") == "description")
-            val description = if (descriptionTag.isEmpty) "" else descriptionTag.map(e => e.attr("content")).head
-            val links: List[URL] = doc.getElementsByTag("a").asScala.map(e => e.attr("href")).filter(s =>
-              urlValidator.isValid(s)).map(link => (new URL(link))).toList
-            SiteMap.tree += Tuple2(url.toString, PageData(url, title, links))
-            links.map(a => self ! Visit(a))
-          }
+      if (!SiteMap.tree.contains(url.toString) && url.toString.contains("wiprodigital.com") && !(url.toString.contains("facebook")) &&
+        !(url.toString.contains("twitter")) && !(url.toString.contains("linkedin"))) {
+        println(s"url::${url.toString}")
+        val response = Jsoup.connect(url.toString).ignoreContentType(true)
+          .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1").execute()
+        val contentType: String = response.contentType
+        if (contentType.startsWith("text/html")) {
+          val doc = response.parse()
+          val title: String = doc.getElementsByTag("title").asScala.map(e => e.text()).head
+          val descriptionTag = doc.getElementsByTag("meta").asScala.filter(e => e.attr("name") == "description")
+          val description = if (descriptionTag.isEmpty) "" else descriptionTag.map(e => e.attr("content")).head
+          val links: List[URL] = doc.getElementsByTag("a").asScala.map(e => e.attr("href")).filter(s =>
+            urlValidator.isValid(s)).map(link => (new URL(link))).toList.distinct
+          SiteMap.tree += Tuple2(url.toString, PageData(url, title, links))
+          links.filter(url => (!SiteMap.tree.contains(url.toString) && url.toString.contains("wiprodigital.com") && !(url.toString.contains("facebook")) &&
+            !(url.toString.contains("twitter")) && !(url.toString.contains("linkedin")))).map(a => self ! Visit(a))
         }
+      }
   }
 
   override def postStop(): Unit = {
-    println("InsidePostStop:::")
+    println("SiteMap prepared till now >>>", SiteMap.tree)
   }
 }
 
